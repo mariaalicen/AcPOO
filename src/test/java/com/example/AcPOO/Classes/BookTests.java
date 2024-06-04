373ab3dd9af3762b231734165e66b856d4d5390d
mariaalicen <mariaalicen12@outlook.com>
2024-06-04 15:55:29 -0300

    iniciando teste de unidade

--- src/test/java/com/example/AcPOO/Classes/BookTests.java
+++ src/test/java/com/example/AcPOO/Classes/BookTests.java
@@ -1,23 +1,41 @@
 package com.example.AcPOO.Classes;
 
+import com.exemple.model.BookModel;
 import com.exemple.repository.BookRepository;
 import com.exemple.service.BookService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+//Configurando mockito
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+//Configurando padrõesJava das classeBook
+import java.util.List;
+import java.util.Optional;
+import java.util.Arrays;
+import java.util.UUID;
+//Configurando importações staticas
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class BookTests {
+public class
+@InjectMocks
+private BookService bookService;
+@Mock
+private BookRepository bookRepository;
 
-    @InjectMocks
-    BookService
+@BeforeEach
+public void setUp(){
+    MockitoAnnotations.openMocks(this);
+}
+@Test
+public void testLivroExiste(){
+    UUID idbook = UUID.randomUUI();
+    BookModel =
+}
 
-    @Mock
-    BookRepository repository
-
-            @Test
-    void
 
 }
